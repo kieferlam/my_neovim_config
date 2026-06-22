b87e8dcc1b3686d211349b26280638e687357f39
Kiefer Lam <kieferlam@gmail.com>
2026-06-22 18:59:59 +0100

    install mason and setup diagnostics

--- init.lua
+++ init.lua
@@ -79,5 +79,29 @@ vim.pack.add({
     "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/folke/trouble.nvim",
 })
+require("trouble").setup({})
+
+vim.pack.add({
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+})
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    -- add more here, e.g. "ts_ls", "pyright", "rust_analyzer"
+  },
+  automatic_enable = true,
+})
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+    severity_sort = true,
+})
 
 dofile(config_path .. "/keymaps.lua")
